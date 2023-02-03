7f314404d593829cb86588f022aa193c832d2b8c
Georgie Abreu <abreu.georgie@gmail.com>
2023-02-03 23:28:11 +0100

    filtre ok sauf tout

--- FrontEnd/scripts/main2.js
+++ FrontEnd/scripts/main2.js
@@ -5,7 +5,7 @@ async function callApiWorks() {
 	}
 	const data = await response.json();
     console.log("Les donnees", data)
-    return (data);
+    return data
 }
 
 function genererFigure(work) {
@@ -32,30 +32,36 @@ function genererFigure(work) {
 console.log("a");
 
 callApiWorks().then((figures) => {
+    console.log("c", figures);
     const filtres = new Set(["Tout"]); 
+
+    console.log("d", filtres);
 
     figures.forEach((figure) => {
         filtres.add(figure.category.name);
         genererFigure(figure);
     });
+    console.log("categorie", filtres); 
 
+    
     filtres.forEach((filtre) => { 
         const boutonFiltre = document.createElement("button");
-        const conteneurFiltres = document.querySelector("#filtres");
+        const conteneurFiltres = document.querySelector("#filtres"); // a mettre dans une fonction car sinon est répété 4 fois
 
         boutonFiltre.innerText = filtre; 
         conteneurFiltres.appendChild(boutonFiltre);
 
+        console.log("conteneurfiltres", conteneurFiltres);
 
         boutonFiltre.addEventListener("click", () => {
+            console.log("e", filtre);
             const projetsFiltrees = figures.filter((figure) => {
-                return figure.category.name === filtre;
+                return figure.category.name == filtre;
             });
-            document.querySelector(".gallery").innerHTML = "";
-            console.log("z", figures)
-            projetsFiltrees.forEach((figure) => {
-                genererFigure(figure);
-            });
+            console.log("h", figures, projetsFiltrees, "j", figures);
+            projetsFiltrees.forEach(genererFigure);
+            console.log("k", figures, "l", projetsFiltrees, typeof projetsFiltrees, typeof figures);
+
         });
     });
 });

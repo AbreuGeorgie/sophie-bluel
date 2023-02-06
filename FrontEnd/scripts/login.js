e6555ecb86bbf252b1f924c75fdca4f67e209343
Georgie Abreu <abreu.georgie@gmail.com>
2023-02-06 12:01:26 +0100

    formulaire envoyé

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,8 +1,5 @@
-let user = {
-    email: " ",
-    password: " "
-  }
-  
+
+async function callApilogin(user) {
   let response = await fetch('http://localhost:5678/api/users/login', {
     method: 'POST',
     headers: {
@@ -12,8 +9,34 @@ let user = {
   });
   
   let result = await response.json();
-  alert(result.message);
+  // alert(result.message);
+  console.log("a", result)
+  return result
+};
+  
+const formulaire = document.querySelector(".form-login");
 
-  document.querySelector("#connecter").addEventListener("click", function(){
-    
-  }
+formulaire.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let user = {
+    email: (event.target.querySelector("#email").value),
+    password: event.target.querySelector("#pass").value
+  };
+  console.log("user", user)
+  callApilogin(user).then((res)=>{
+    document.location.href="./index.html";
+  })
+  
+    .catch();
+
+  //formulaire.submit();
+
+
+});
+  
+      /* window.addEventListener("load", (event) => {
+        console.log("page is fully loaded");
+      }); */
+  
+   
+  //{"userId":1,"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY3NTUxNzE1OSwiZXhwIjoxNjc1NjAzNTU5fQ.WBBRIWsV4-cp3wY84-k83ZJ6escehX3cmrPRu1xVzXU"}

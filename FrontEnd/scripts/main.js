ba0970f70fa4018359cf8669d1e46e2955a24bfc
Georgie Abreu <abreu.georgie@gmail.com>
2023-03-13 11:57:00 +0100

    main.js : import fonction logout de logout.js et ajout de commentaires

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -2,40 +2,36 @@
 import { callApiWorks, genererFigure } from "./work.js";
 import { genererFiltres } from "./filtres.js";
 import { openModal } from "./modal.js";
+import { logout } from "./logout.js";
 
 //Affichage des éléments une fois la page chargée
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', function (event) {
     let token = window.sessionStorage.getItem("token")
+    
+    // si connecté alors:
     if (token !== null) {
-        console.log(token, 'connecté')
-        // si connecté alors:
+        //modification du bouton login en logout
         const boutonLogin = document.querySelector(".login-logout");
         boutonLogin.innerHTML = "logout";
+        //mode edition affiché
         document.getElementById("mode-edition").style.display = "flex";
+        //bouton modifié affiché
         document.getElementById("modifier").style.display = "block";
+        //bouton modifié (sous photo) affiché
         document.getElementById("modifier-photo").style.display = "block";
     };
 
-    callApiWorks().then((figures) => {
-        figures.forEach((figure) => {
-            genererFigure(figure); // pour chaque projet => generer projet
+    callApiWorks().then(function (figures) {
+        figures.forEach(function (figure) {
+            // pour chaque projet => generer projet
+            genererFigure(figure);
         });
-        genererFiltres();
-        
+        // generer les filtres
+        genererFiltres();  
     });
 
-    const boutonLogin = document.querySelector(".login-logout"); //reccuperation du bouton Login
-    //deconnexion
-    boutonLogin.addEventListener("click", function (e) {
-        if (window.sessionStorage.getItem("token") != null) {
-            e.preventDefault() // ne pas rediriger
-            window.sessionStorage.removeItem("token"); //supprime le token
-            boutonLogin.innerHTML = "login";
-            document.getElementById("mode-edition").style.display = null;
-            document.getElementById("modifier").style.display = null;
-            document.getElementById("modifier-photo").style.display = null;
-        }
-    });
+    //deconnection
+    logout();
 
     //fonction qui va appeler a l'ouverture de la modale lors du clique sur le bouton modifier
     document.querySelector(".js-modal").addEventListener("click", (e) => {
@@ -46,3 +42,4 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
 });
+
